c95aef162840892f47ff8014eb408d75ff534043
Cliff Gallagher <scliffordgallagher@gmail.com>
2022-07-13 16:35:55 -0500

    code correct through 194

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 import useHttp from './hooks/use-http';
@@ -6,22 +6,23 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = useCallback(tasksObj => {
-    const loadedTasks = [];
-
-      for (const taskKey in tasksObj) {
-        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-  }, []);
-
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
+
+    const transformTasks = tasksObj => {
+      const loadedTasks = [];
+  
+        for (const taskKey in tasksObj) {
+          loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+        }
+  
+        setTasks(loadedTasks);
+    };
+    
     fetchTasks({
       url: 'https://customhooksrealistic2-default-rtdb.firebaseio.com/tasks.json'
-    });
+    }, transformTasks);
   }, [fetchTasks]);
 
   const taskAddHandler = (task) => {

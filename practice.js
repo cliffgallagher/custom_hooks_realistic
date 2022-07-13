2a9167d613ccd50c4c9a6a8ded538b932db916cb
Cliff Gallagher <scliffordgallagher@gmail.com>
2022-07-13 16:28:23 -0500

    app works with useCallback

---
+++ practice.js
@@ -0,0 +1,12 @@
+const practice = () => {
+    const myArray = [5, 6, 7];
+
+    const [one, two, three, four] = myArray;
+
+    console.log(one);
+    console.log(two);
+    console.log(three);
+    console.log(four);
+}
+
+practice();
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 import useHttp from './hooks/use-http';
@@ -6,7 +6,7 @@ import useHttp from './hooks/use-http';
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = tasksObj => {
+  const transformTasks = useCallback(tasksObj => {
     const loadedTasks = [];
 
       for (const taskKey in tasksObj) {
@@ -14,15 +14,15 @@ function App() {
       }
 
       setTasks(loadedTasks);
-  }
+  }, []);
 
-  const { isLoading, error, sendRequest: fetchTasks } = useHttp({
-    url: 'https://customhooksrealistic2-default-rtdb.firebaseio.com/tasks.json'
-  }, transformTasks);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp(transformTasks);
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks({
+      url: 'https://customhooksrealistic2-default-rtdb.firebaseio.com/tasks.json'
+    });
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
